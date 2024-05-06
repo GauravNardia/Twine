7bafd8648eef684d2b2d0ad4a1cdd8de10bb8b70
Gaurav Nardia <gauravnardia07@gmail.com>
2024-05-06 09:28:50 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+
+const nextConfig = {
+  experimental: {
+    serverComponents: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: [
+      "img.clerk.com",
+      "images.clerk.dev",
+      "uploadthing.com",
+      "placehold.co",
+    ],
+  },
+};
 
 export default nextConfig;
